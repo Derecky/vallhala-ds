7e9ce019ce650bc4f3dc33e899dfa68dd6aef61b
Derecky Costa <dereckycosta@gmail.com>
2023-02-20 19:05:42 -0300

    chore: Add stitches

---
+++ packages/react/src/index.tsx
@@ -0,0 +1,5 @@
+export * from '.'
+
+export function APP() {
+  return <h1>Hello world</h1>
+}
---
+++ packages/react/src/lib/stitches/stiches.config.ts
@@ -0,0 +1,37 @@
+import { createStitches, defaultThemeMap } from '@stitches/react'
+
+import {
+  colors,
+  fontSizes,
+  fontWeights,
+  fonts,
+  lineHeights,
+  radii,
+  space,
+} from '@vallhala/core'
+
+export const {
+  config,
+  createTheme,
+  css,
+  getCssText,
+  globalCss,
+  keyframes,
+  styled,
+  theme,
+} = createStitches({
+  theme: {
+    colors,
+    fontSizes,
+    fontWeights,
+    fonts,
+    lineHeights,
+    radii,
+    space,
+  },
+  themeMap: {
+    ...defaultThemeMap,
+    width: 'space',
+    height: 'space',
+  },
+})
